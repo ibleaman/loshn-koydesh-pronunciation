7f0cac5f6de178bbcd7e80e2c8d906bfa7552124
Isaac L. Bleaman <bleaman@berkeley.edu>
2021-07-23 09:46:08 -0700

    Incorporate Niborski's alphabetization, more or less

--- orthographic_to_phonetic.py
+++ orthographic_to_phonetic.py
@@ -4,8 +4,8 @@ phonetic_list = 'phonetic-to-orthographic.txt'
 # output_file
 orthographic_list = 'orthographic-to-phonetic.txt'
 
-
 import csv
+import re
 
 with open(phonetic_list, 'r') as file:
     data = file.read()
@@ -14,15 +14,44 @@ phonetic_forms = data.split('\n')
 
 orthographic_forms = dict()
 
+# FOR ALPHABETIZATION
+# map digraphs to precombined characters, only for those relevant to alphabetization
+precombined = {
+    ('בֿ','בֿ'),
+    ('כּ','כּ'),
+    ('פּ','פּ'),
+    ('פֿ','פֿ'),
+    ('שׂ','שׂ'),
+    ('תּ','תּ'),
+}
+diacritics_to_remove = 'ִַָּ' + '…׳״'
+alphabetically_equivalent = {
+    ('ך', 'כ'),
+    ('ם', 'מ'),
+    ('ן', 'נ'),
+    ('ף', 'פֿ'),
+    ('ץ', 'צ'),
+    ('ײ', 'יי'),
+}
+# new alphabetical order; based on Niborski (1999:xiii)
+alphabet = ' ־אבבֿגדהוזחטיכּכלמנסעפּפֿצקרששׂתּת'
+
 for entry in phonetic_forms:
     phon = entry.split('\t')[0]
     orth = entry.split('\t')[1]
 
-    if orth not in orthographic_forms:
-        orthographic_forms[orth] = []
+    alph = orth # a simplified representation used for alphabetization
+    for pair in precombined:
+        alph = re.sub(pair[0], pair[1], alph)
+    for character in diacritics_to_remove:
+        alph = re.sub(character, '', alph)
+    for pair in alphabetically_equivalent:
+        alph = re.sub(pair[0], pair[1], alph)
 
-    orthographic_forms[orth].append(phon)
+    if alph not in orthographic_forms:
+        orthographic_forms[alph] = {'orth': orth, 'phon': list()}
+    orthographic_forms[alph]['phon'].append(phon)
 
 with open(orthographic_list, 'w') as text_file:
-    for orth, phon in sorted(orthographic_forms.items()):
-        text_file.write(orth + '\t' + ','.join(phon) + '\n')
+    for alph in sorted(orthographic_forms.keys(), key=lambda word: [alphabet.index(c) for c in word]):
+        text_file.write(orthographic_forms[alph]['orth'] + '\t' + ','.join(orthographic_forms[alph]['phon']) + '\n')
